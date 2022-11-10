1c6e59d248217776fd8b93508d720784cb752097
amontalb <amontalb@student.42lyon.fr>
2022-11-10 17:17:07 +0100

    ok

--- libft.h
+++ libft.h
@@ -43,4 +43,5 @@ char		*ft_strdup(const char *s1);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char 		*ft_strjoin(char const *s1, char const *s2);
+int			ft_atoi(char *str);
 #endif
