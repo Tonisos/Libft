e7cecd1ce2072be2ce743cd292dd112233ae444d
Tonisos <montaltonio@gmail.com>
2022-11-11 16:34:59 +0100

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antoinemontalbetti <antoinemontalbetti@    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 14:21:25 by antoinemont       #+#    #+#             */
+/*   Updated: 2022/11/11 16:34:37 by antoinemont      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_isinset(char s1, char const *set)
+{
+    int i;
+    
+    i = 0;
+    while (set[i] != '\0')
+    {
+        if (s1 == set[i])
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+int ft_countstart(char const *s1, char const *set)
+{
+    int i;
+    
+    i = 0;
+    while (ft_isinset(s1[i], set))
+            i++;
+    return (i);
+}
+
+
+int ft_countend(char const *s1, char const *set)
+{
+    int i;
+
+    i = ft_strlen(s1) - 1;
+    while (ft_isinset(s1[i], set))
+        i--;
+    return (i);
+}
+
+char    *ft_strtrim(char const *s1, char const *set)
+{
+    int i;
+    int count;
+    int j;
+    char *trim;
+
+    j = 0;
+    count = ft_countstart(s1, set) + ft_countend(s1, set);
+    i = ft_countstart(s1, set);
+
+    trim =(char *) malloc(sizeof(char) * count);
+    while (i < ft_countend(s1, set) + 1)
+    {
+        trim[j] = s1[i];
+        i++;
+        j++;
+    }
+    trim[j] = '\0';
+    return (trim);
+}
