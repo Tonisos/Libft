e641cc97617ce0b4138886a9c389a3cb5d4a4fd9
Tonisos <montaltonio@gmail.com>
2022-11-15 19:43:41 +0100

    map

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,23 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amontalb <amontalb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: antoinemontalbetti <antoinemontalbetti@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 15:52:37 by amontalb          #+#    #+#             */
-/*   Updated: 2022/11/15 16:56:48 by amontalb         ###   ########.fr       */
+/*   Updated: 2022/11/15 19:43:31 by antoinemont      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*map;
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*begin;
+	t_list	*newnode;
 
-// 	map = malloc(ft_lstsize(lst) * sizeof(lst));
-// 	while (lst)
-// 	{
-// 		map = 
-// 	}
-
-// }
+	while (lst)
+	{
+		begin = ft_lstnew(f(lst->content));
+		if (!begin)
+		{
+			ft_lstdelone(begin, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&begin, newnode);
+		lst = lst->next;
+	}
+	return (begin);
+}
