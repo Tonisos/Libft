cf0ad1bec98bfe5db2de060fc7329cb34a5f11b4
amontalb <amontalb@student.42lyon.fr>
2022-11-16 14:11:26 +0100

    ok

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: amontalb <amontalb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:21:25 by antoinemont       #+#    #+#             */
-/*   Updated: 2022/11/16 13:59:39 by amontalb         ###   ########.fr       */
+/*   Updated: 2022/11/16 14:10:35 by amontalb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*trim;
 	size_t	i;
 	size_t	j;
-	size_t	k;
 
 	if (!s1)
 		return (NULL);
@@ -25,7 +24,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return ((char *)s1);
 	i = 0;
 	j = ft_strlen(s1);
-	k = 0;
 	while (s1[i] && ft_strchr((char *)set, (int)s1[i]) != 0)
 		i++;
 	while ((j > 0) && ft_strrchr((char *)set, (int)s1[j]) != 0)
